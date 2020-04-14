813539663e5f84deffd7953648c6c0aa64c7d941
hansbuehler <github@hans.buehler.london>
2020-04-15 00:48:02 +0100

    Removed PyPi package management directories.

--- .ipynb_checkpoints/upload_pip-checkpoint.bat
+++
@@ -1,6 +0,0 @@
-REM https://packaging.python.org/tutorials/packaging-projects/
-
-cd C:\Users\hansb\iCloudDrive\Python3\packages\cdxbasics
-python setup.py sdist bdist_wheel
-python -m twine upload dist/*
-del dist/*
