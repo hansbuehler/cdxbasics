531ff056686e1e61a186992a5fe1523c19771d3c
hansbuehler <github@hans.buehler.london>
2023-01-04 22:30:12 +0100

    Code base 0.2.5

--- setup.py
+++ setup.py
@@ -2,12 +2,35 @@
 
 import setuptools
 
+init_file = "./cdxbasics/__init__.py"
+with open(init_file, "rt") as fh:
+    init = fh.read()
+    find_str = '__version__ = "'
+    i = init.find(find_str)
+    assert i>0, "Error: cannot find string '%s' in cdxbasics/__init__.py" % find_str
+    i += len(find_str)
+    data = init[i:]
+    start = init[:i]
+    i = data.find('"')
+    assert i>=0, "Error: cannot find closing quotation marks"
+    end = data[i:]
+    data = data[:i]
+    x = [ int(i) for i in data.split('.') ]
+    assert len(x) == 3, "Error: found %s not a version ID" % data.split(".")
+    x[-1] += 1
+    version = "%ld.%ld.%ld" % ( x[0], x[1], x[2] )
+    print("Upgraded package version to %s in %s" % (version,init_file))
+    new_file = start + version + end
+    
+with open(init_file, "wt") as fh:
+    fh.write(new_file)
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="cdxbasics", 
-    version="0.2.4",    # found in cdxbasics/__init__.py
+    version=version,    # found in cdxbasics/__init__.py
     author="Hans Buehler",
     author_email="github@buehler.london",
     description="Basic Python tools",
