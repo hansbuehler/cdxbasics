cfbf46425dd3448ee43cfa3b9b4eaf1c06a42f86
hans <github@buehler.london>
2023-05-29 18:13:16 +0100

    Code base 0.2.49

--- cdxbasics/__init__.py
+++ cdxbasics/__init__.py
@@ -4,7 +4,7 @@ Created on June 2022
 @author: hansb
 """
 
-__version__ = "0.2.48"  # auto-updated by setup.py
+__version__ = "0.2.49"  # auto-updated by setup.py
 
 from .util import *
 from .logger import Logger
